2642b6bdb4390d605de45a4bb586638a96a29fb3
aminulz <cn9539@gmail.com>
2021-11-24 15:04:25 +0600

    Practice Programs

--- src/FirstCLass.java
+++ src/FirstCLass.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class FirstCLass {
     public static void main(String[] args) {
-        System.out.println("Some Text");
-        System.out.print("Second Line");
-        System.out.println("Third Line");
-        System.out.println("Fourth Line");
+        Scanner ScannedObject = new Scanner(System.in);
+        System.out.println("Enter Kilometer Value");
+        int num1 = ScannedObject.nextInt();
+
+        double mile = num1 * 0.62137;
+        double meter = num1 * 1000;
+
+        System.out.println(num1+" Kilometer = " +mile +" Miles");
+        System.out.println(num1+" Kilometer = " +meter +" Meters");
+
     }
 }
