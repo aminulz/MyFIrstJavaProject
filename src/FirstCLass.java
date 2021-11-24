d352870a3653e44383d0e2a69051b51fb8d88e9d
aminulz <cn9539@gmail.com>
2021-11-24 10:55:53 +0600

    Init

---
+++ src/FirstCLass.java
@@ -0,0 +1,7 @@
+public class FirstCLass {
+    public static void main(String[] args) {
+        System.out.println("Some Text");
+        System.out.print("Second Line");
+        System.out.println("Third Line");
+    }
+}
