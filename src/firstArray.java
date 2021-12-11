58782e9c2600d49bed430c667fc42f0fb820c786
aminulz <cn9539@gmail.com>
2021-12-11 16:16:02 +0600

    Array moderate

--- src/firstArray.java
+++ src/firstArray.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 public class firstArray {
     public static void main(String[] args) {
         int [] oldArray = {5, 15, 25, 35, 45, 55, 65, 75, 85, 95};
-        for (int counter: oldArray) {
-            System.out.println(counter);
-        }
-        int [] newArray = Arrays.copyOf(oldArray, 6);
-        newArray[5] = 56;
-        System.out.println("Modified array");
+        int [] newArray = Arrays.copyOf(oldArray, oldArray.length);
+        newArray[5] = 556;
+        System.out.println("Old array");
         for (int c2:oldArray) {
-            System.out.println(c2);
+            System.out.print(c2 +" ");
         }
-        System.out.println();
+        System.out.println("\nNew Array");
         for (int c3:newArray) {
-            System.out.println(c3);
+            System.out.print(c3 +" ");
         }
     }
 }
