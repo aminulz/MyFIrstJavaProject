d8d3d5509d8c0085e81b50702a5cb7694d1e45de
aminulz <cn9539@gmail.com>
2021-12-11 16:12:07 +0600

    Array moderate

--- src/firstArray.java
+++ src/firstArray.java
@@ -1,26 +1,21 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class firstArray {
     public static void main(String[] args) {
-//        int [] myArray = {10, 12, 14, 16, 18, 20};
-//        int [] newArray = myArray;
-////        System.out.println(stringArr[1]+" "+myArray[2]);
-//        for(int i=0; i<5; i++){
-//            System.out.print(newArray[i]+" ");
-//        }
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter array length: ");
-        int arrLength = input.nextInt();
-        int [] inputArray = new int[arrLength];
-        for(int i=0; i < arrLength; i++) {
-            System.out.println("Enter array elements: ");
-            inputArray[i] = input.nextInt();
+        int [] oldArray = {5, 15, 25, 35, 45, 55, 65, 75, 85, 95};
+        for (int counter: oldArray) {
+            System.out.println(counter);
         }
-        System.out.print("Here are values: ");
-        for (int j=0; j<arrLength; j++){
-            System.out.print(inputArray[j]+" ,");
+        int [] newArray = Arrays.copyOf(oldArray, 6);
+        newArray[5] = 56;
+        System.out.println("Modified array");
+        for (int c2:oldArray) {
+            System.out.println(c2);
+        }
+        System.out.println();
+        for (int c3:newArray) {
+            System.out.println(c3);
         }
     }
-
-
 }
