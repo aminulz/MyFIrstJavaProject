176df7a8033dd9daab948f7ea655e8987e75af8d
aminulz <cn9539@gmail.com>
2021-12-12 12:10:52 +0600

    1D array with intermediate commands

--- src/firstArray.java
+++ src/firstArray.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class firstArray {
     public static void main(String[] args) {
         int [] oldArray = {5, 15, 25, 35, 45, 55, 65, 75, 85, 95};
-        int [] newArray = Arrays.copyOf(oldArray, oldArray.length);
+        int [] newArray = Arrays.copyOf(oldArray, oldArray.length); //Making a copy of old array
         newArray[5] = 556;
         System.out.println("Old array");
         for (int c2:oldArray) {
@@ -14,5 +14,7 @@ public class firstArray {
         for (int c3:newArray) {
             System.out.print(c3 +" ");
         }
+
+        System.out.println("\nSum of array 1 is: " + Arrays.stream(oldArray).sum()); //Sum of oldArray using Arrays.stream
     }
 }
